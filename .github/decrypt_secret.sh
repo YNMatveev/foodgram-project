8c6d7289b52530012ac79ea234ea2cb13344d911
Yuri N. Matveev <ynmatveev@yandex.ru>
2021-05-27 02:13:29 +0300

    test workflow

--- .github/decrypt_secret.sh
+++ .github/decrypt_secret.sh
@@ -5,4 +5,4 @@ mkdir $HOME/secrets
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_SECRET_PASSPHRASE" \
---output $HOME/secrets/.env .env.gpg
+--output $HOME/.env .env.gpg
