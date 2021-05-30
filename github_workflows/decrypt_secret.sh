7182703fc72a509c94fffe4d5214a43bf3ac44f9
Yuri N. Matveev <ynmatveev@yandex.ru>
2021-05-31 00:15:41 +0300

    all_done

--- github_workflows/decrypt_secret.sh
+++ github_workflows/decrypt_secret.sh
@@ -5,3 +5,6 @@
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$GPG_SECRET_PASSPHRASE" \
 --output $GITHUB_WORKSPACE/.env .env.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_SECRET_PASSPHRASE" \
+--output $GITHUB_WORKSPACE/dhparam-2048.pem dhparam-2048.pem.gpg
